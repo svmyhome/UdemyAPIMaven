339769e3ae0d3b2dc2d6f31398a1d3f2d3533e42
Sarychev Vladimir <svmyhome@mail.ru>
2021-02-17 22:22:59 +0300

    Aded annotation with step

--- src/test/java/zeroTest.java
+++ src/test/java/zeroTest.java
@@ -1,10 +1,19 @@
+import io.qameta.allure.*;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.*;
 
+@Epic("Работа с ЛК пользователя")
+@Feature(value = "3.8 Получение справочной информации")
 public class zeroTest {
 
+
     @Test
+//        @Step("Проверка разности числа {num1} и числа {num2}")
+    @DisplayName("Получения информации о профиле {logE}")
+    @Description(value = "3.8.2 Для получения информации о своём профиле используется метод GET")
+    @Severity(SeverityLevel.CRITICAL)
     public void MyFirstTest() {
         given().log().uri().
                 when().get("https://swapi.dev/api/people/1").
@@ -15,6 +24,6 @@ public class zeroTest {
     public void SecondTest() {
         given().log().uri().
                 when().get("https://swapi.dev/api/people/2").
-                then().log().body().statusCode(201);
+                then().log().body().statusCode(200);
     }
 }
