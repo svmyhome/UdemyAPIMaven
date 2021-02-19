b062e4a5d96ac7326bd536a8a2e95efc7c696cad
Sarychev Vladimir <svmyhome@mail.ru>
2021-02-19 19:06:15 +0300

    Added Methods: Get with query params Put

---
+++ src/test/java/jasonPaseHolder.java
@@ -0,0 +1,34 @@
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class jasonPaseHolder {
+
+    @Test
+    public void GET(){
+        given().queryParam("postId", "1").log().uri().
+                when().get("https://jsonplaceholder.typicode.com/comments").
+                then().log().body().statusCode(200);
+    }
+
+    @Test
+    public void PUT(){
+        String jsonBody = "{\n" +
+                "\"userId\":1,\n" +
+                "\"id\":1,\n" +
+                "\"title\":\"sunt aut facere repellat provident occaecati excepturi optio reprehenderit\",\n" +
+                "\"body\":\"quia et suscipit\\nsuscipit recusandae consequuntur expedita et cum\\nreprehenderit molestiae ut ut quas totam\\nnostrum rerum est autem sunt rem eveniet architecto\"\n" +
+                "}";
+        given().body(jsonBody).log().uri().
+                when().put("https://jsonplaceholder.typicode.com/posts/1").
+                then().log().body().statusCode(200);
+    }
+
+    @Test
+    public void DELETE(){
+        given().log().uri().
+                when().delete("https://jsonplaceholder.typicode.com/posts/1").
+                then().log().body().statusCode(200);
+    }
+
+}
