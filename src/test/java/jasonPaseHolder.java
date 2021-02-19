05364ab79eea1023f962dbd965c1c4fa1831f3dc
Sarychev Vladimir <svmyhome@mail.ru>
2021-02-19 19:30:25 +0300

    Added Allure Report

--- src/test/java/jasonPaseHolder.java
+++ src/test/java/jasonPaseHolder.java
@@ -1,3 +1,4 @@
+import io.qameta.allure.restassured.AllureRestAssured;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
@@ -6,9 +7,9 @@ public class jasonPaseHolder {
 
     @Test
     public void GET(){
-        given().queryParam("postId", "1").log().uri().
+        given().filter(new AllureRestAssured()).queryParam("postId", "1").log().uri().
                 when().get("https://jsonplaceholder.typicode.com/comments").
-                then().log().body().statusCode(200);
+                then().log().body().statusCode(201);
     }
 
     @Test
@@ -19,14 +20,14 @@ public class jasonPaseHolder {
                 "\"title\":\"sunt aut facere repellat provident occaecati excepturi optio reprehenderit\",\n" +
                 "\"body\":\"quia et suscipit\\nsuscipit recusandae consequuntur expedita et cum\\nreprehenderit molestiae ut ut quas totam\\nnostrum rerum est autem sunt rem eveniet architecto\"\n" +
                 "}";
-        given().body(jsonBody).log().uri().
+        given().filter(new AllureRestAssured()).body(jsonBody).log().uri().
                 when().put("https://jsonplaceholder.typicode.com/posts/1").
                 then().log().body().statusCode(200);
     }
 
     @Test
     public void DELETE(){
-        given().log().uri().
+        given().filter(new AllureRestAssured()).log().uri().
                 when().delete("https://jsonplaceholder.typicode.com/posts/1").
                 then().log().body().statusCode(200);
     }
--- src/test/java/zeroTest.java
+++ src/test/java/zeroTest.java
@@ -1,4 +1,5 @@
 import io.qameta.allure.*;
+import io.qameta.allure.restassured.AllureRestAssured;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -15,14 +16,14 @@ public class zeroTest {
     @Description(value = "3.8.2 Для получения информации о своём профиле используется метод GET")
     @Severity(SeverityLevel.CRITICAL)
     public void MyFirstTest() {
-        given().log().uri().
+        given().filter(new AllureRestAssured()).log().uri().
                 when().get("https://swapi.dev/api/people/1").
                 then().log().body().statusCode(200);
     }
 
     @Test
     public void SecondTest() {
-        given().log().uri().
+        given().filter(new AllureRestAssured()).log().uri().
                 when().get("https://swapi.dev/api/people/2").
                 then().log().body().statusCode(200);
     }
